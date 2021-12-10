f4925308ab8a024e7ce3373bbff76c68c028d3a1
Daniel Zupan <GiovanniBattista@users.noreply.github.com>
2021-12-10 07:19:18 +0100

    Kurstag 8: Styling own toggle button (css only)

---
+++ src/app/modules/shared/components/toggle/toggle.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+let counter = 1;
+
+@Component({
+  selector: 'app-toggle',
+  templateUrl: './toggle.component.html',
+  styleUrls: ['./toggle.component.css']
+})
+export class ToggleComponent {
+
+  id = counter++;
+
+  @Input() disabled = false;
+  @Input() value = false;
+  @Output() valueChange = new EventEmitter<boolean>();
+
+  constructor() { }
+
+}
